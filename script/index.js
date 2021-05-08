ae4208ac7f07be0927b40e67921e634613427d2c
leonardo <leonardodimarchi@outlook.com>
2021-05-08 19:44:06 -0300

    feat(home): added initial loading state

---
+++ script/index.js
@@ -0,0 +1,12 @@
+//Loading effect
+setTimeout(() => {
+    $(".loading-icon").hide();
+    $(".loading-container").css("height",0);
+
+    $("#htmlTag").css("height","unset !important");
+
+    $("#ipt-navbar").show();
+    $("#ipt-footer").show();
+    $(".home-content").show();
+    $(".home-content").css("animation","fadeIn 2s");
+},1500);
