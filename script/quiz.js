3e456df43bc540bdbd4f9312bd38219bf9d96249
leonardo <leonardodimarchi@outlook.com>
2021-05-15 00:37:55 -0300

    feat(quiz): added quiz structure and validation along with solar content questions

--- script/quiz.js
+++ script/quiz.js
@@ -1,9 +1,11 @@
+const currentQuestion = $('#quiz-current-question');
+let currentQuestionOptionNumber = 1;
+
 const questionsOptions = [
     $('#question1'),
     $('#question2'),
     $('#question3'),
     $('#question4'),
-    $('#question5'),
 ];
 
 const alternativeOptions = [
@@ -13,24 +15,96 @@ const alternativeOptions = [
     $('#alternative4'),
 ];
 
-const currentEnergyQuestions = []
+const alternativeOptionsButtons = [
+    $('#alternativeButton1'),
+    $('#alternativeButton2'),
+    $('#alternativeButton3'),
+    $('#alternativeButton4'),
+]
+
+let currentEnergyQuestions;
+loadCurrentEnergyQuestions();
+
+setTimeout(() => {
+    selectQuizOption(1);
+
+    $('#question1').on('click',()=>{selectQuizOption(1)});
+    $('#question2').on('click',()=>{selectQuizOption(2)});
+    $('#question3').on('click',()=>{selectQuizOption(3)});
+    $('#question4').on('click',()=>{selectQuizOption(4)});
+},1000)
+
 
 function selectQuizOption(option) {    
+    currentQuestionOptionNumber = option;
+
     questionsOptions.forEach(question=> {
         question.removeClass(' quiz-select-question-option-active');
     });
 
     questionsOptions[Number(option)-1].addClass('quiz-select-question-option-active');
+
+    currentQuestion.html(currentEnergyQuestions.questions[option-1]);
+
+    for(let contador=0; contador < alternativeOptions.length; contador++) {
+
+        switch(option) {
+            case 1:
+                alternativeOptions[contador].html(currentEnergyQuestions.alternatives.question1[contador])
+                break;
+            case 2:
+                alternativeOptions[contador].html(currentEnergyQuestions.alternatives.question2[contador])
+                break;
+            case 3:
+                alternativeOptions[contador].html(currentEnergyQuestions.alternatives.question3[contador])
+                break;
+            case 4:
+                alternativeOptions[contador].html(currentEnergyQuestions.alternatives.question4[contador])
+                break;
+        }
+    }
+
+    alternativeOptionsButtons.forEach(alternative=> {
+        alternative.removeClass('quiz-alternative-active');
+        alternative.removeClass('quiz-alternative-wrong');
+    });
+}
+
+function checkCorrectAnswer(option) {
+    if(option === currentEnergyQuestions.correctAlternatives[currentQuestionOptionNumber-1])
+        return true;
+
+    return false;
 }
 
 function selectQuizAlternative(option) {
-    alternativeOptions.forEach(alternative=> {
+    alternativeOptionsButtons.forEach(alternative=> {
         alternative.removeClass('quiz-alternative-active');
+        alternative.removeClass('quiz-alternative-wrong');
     });
 
-    //if(checkCorrectAnswer(option))
-    alternativeOptions[Number(option)-1].addClass('quiz-alternative-active');
+    if(checkCorrectAnswer(option)) {
+        alternativeOptionsButtons[Number(option)-1].addClass('quiz-alternative-active');
+    } else {
+        alternativeOptionsButtons[Number(option)-1].addClass('quiz-alternative-wrong');
+    }
 }
 
-//function checkCorrectAnswer(option)
+function loadCurrentEnergyQuestions() {
+    $.getJSON('../content/questions.json', (data) => {
+        const paginaEnergiaAtual = localStorage.getItem('paginaAtual')
 
+        if(paginaEnergiaAtual.includes('solar')) {
+            currentEnergyQuestions = data.solarQuestions;
+
+        } else if (paginaEnergiaAtual.includes('eolica')) {
+            currentEnergyQuestions = data.solarQuestions;
+
+        } else if (paginaEnergiaAtual.includes('hidraulica')) {
+            currentEnergyQuestions = data.solarQuestions;
+
+        } else if (paginaEnergiaAtual.includes('maremotriz')) {
+            currentEnergyQuestions = data.solarQuestions;
+        } 
+    }).fail("Ocorreu um erro")
+}
